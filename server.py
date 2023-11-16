baa687d79ddc705e48bf3237c555d7c48f3e0ff8
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-11-16 10:58:17 +0530

    added the reading from text file functionality

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
 from socket import *
 import struct
 
-dns_mapping = {
-    "example.com": "192.168.1.1",
-    "google.com": "8.8.8.8",
-    "yahoo.com": "98.138.219.231",
-    "github.com": "192.30.255.113",
-    "openai.com": "52.44.42.29",
-}
-
-def resolve_dns(query_domain):
-    return dns_mapping.get(query_domain, "Domain not found")
+def get_ip_from_file(filename, query_domain):
+    with open(filename, 'r') as file:
+        for line in file:
+            domain, ip_address = line.strip().split()
+            if domain == query_domain:
+                return ip_address
+    return "Domain not found"
 
 def handle_dns_request(transaction_id, query_domain):
-    ip_address = resolve_dns(query_domain)
+    ip_address = get_ip_from_file(filename,query_domain)
     return transaction_id, ip_address
+
+filename = "dns_mapping.txt"
 
 sname='127.0.0.1'
 sport=8001
@@ -27,7 +26,7 @@ while True:
     transaction_id, query_domain = struct.unpack('!H{}s'.format(len(dns_request_packet) - 2), dns_request_packet)
 
     response_transaction_id, ip_address = handle_dns_request(transaction_id, query_domain.decode())
-    
+
     dns_response_packet = struct.pack('!H{}s'.format(len(ip_address)), response_transaction_id, ip_address.encode())
     ssocket.sendto(dns_response_packet, client_address)
     break
